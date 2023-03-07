abc1ba6207ca771f2b2464ec3687dc3e51f4406d
Maxime <mbinet42@gmail.com>
2023-03-07 18:22:23 +0100

    Move config files to root in ordeer to work with Render

--- bot/config.py
+++ bot/config.py
@@ -2,14 +2,12 @@ import yaml
 import dotenv
 from pathlib import Path
 
-config_dir = Path(__file__).parent.parent.resolve() / "config"
-
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+with open("config.yml", 'r') as f:
     config_yaml = yaml.safe_load(f)
 
 # load .env config
-config_env = dotenv.dotenv_values(config_dir / "config.env")
+config_env = dotenv.dotenv_values("config.env")
 
 # config parameters
 telegram_token = config_yaml["telegram_token"]
